0dc0592c1d973b95f6abeb518476518294789cd9
fuyuxxh <smartstar0703+sub@gmail.com>
2026-03-01 06:27:46 +0900

    test

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import { Client, GatewayIntentBits, Events } from "discord.js";
 import dotenv from "dotenv";
+import fs from "fs";
 import { registerMessageHandler } from "./src/main";
 
-dotenv.config({ path: "./config/token.env" });
+const tokenPath = "./config/token.env";
+
+if (!fs.existsSync(tokenPath)) {
+    console.error(`Error: Make sure the directory "${tokenPath}" exists, or rename "example_token.env" to "token.env".`);
+    process.exit(1);
+}
+
+dotenv.config({ path: tokenPath });
 
 const client = new Client({
     intents: [
@@ -22,7 +30,7 @@ registerMessageHandler(client);
 const token = process.env.BOT_TOKEN;
 
 if (!token) {
-    console.error("Error: Invalid token");
+    console.error("Error: Invalid token. Make sure you have added the token to the file.");
     process.exit(1);
 }
 
